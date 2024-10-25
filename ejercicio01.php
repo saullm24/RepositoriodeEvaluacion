9e4e036bfe04981c92d4eee909aa913dfcc0c36c
saullm24 <patitaslm57@gmail.com>
2024-10-24 19:51:40 -0600

    Add files via upload
    En este trabajo hicimos una numeración del 1 al 100
    también lo hicimos en numero par

---
+++ ejercicio01.php
@@ -0,0 +1,8 @@
+<? php
+$nombre = "Mi nombre es": Lesly Alexandra <BR>
+$apellidomaterno = "Mi apellido materno es": Mercado <BR>
+$apellidopaterno = "Mi apellido paterno es": Mercado <BR>
+
+$exite = true; //Se declara una variable boolean
+
+<?
